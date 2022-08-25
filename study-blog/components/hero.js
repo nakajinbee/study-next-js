bfcf76999f73deb0add5571b091b1d028707834b
nakaji <nakaji.m.0625@gmail.com>
2022-08-25 22:12:39 +0900

    fix type.

--- study-blog/components/hero.js
+++ study-blog/components/hero.js
@@ -3,9 +3,9 @@ import styles from 'styles/hero.module.css'
 export default function Hero({ title, subtitle, imageOn = false }) {
     return (
         <div>
-            <div className="{styles.text}">
-                <h1 className='{styles.title}'>{title}</h1>
-                <p className='{styles.suntitle}'>{subtitle}</p>
+            <div className={styles.text}>
+                <h1 className={styles.title}>{title}</h1>
+                <p className={styles.suntitle}>{subtitle}</p>
                 {imageOn && <figure>[画像]</figure>}
             </div>
         </div>
--- study-blog/components/nav.js
+++ study-blog/components/nav.js
@@ -4,7 +4,7 @@ import styles from 'styles/nav.module.css'
 export default function Nav () {
     return (
         <nav>
-            <ul className="[styles.list}">
+            <ul className={styles.list}>
                 <li>
                     <Link href="/">
                         <a>HOME</a>
